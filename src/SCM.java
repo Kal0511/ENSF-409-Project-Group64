b96660ab90af9fe966c36520df8b4cae5c909199
AdamAbouelhassan <78115573+AdamAbouelhassan@users.noreply.github.com>
2021-03-27 13:06:03 -0600

    Update SCM.java

--- src/SCM.java
+++ src/SCM.java
@@ -1,24 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.sql.*;
+import java.io.*;
 
 public class SCM {
     public static void main(String[] args) {
         Inventory test = new Inventory();
         test.initializeConnection();
-
-        BufferedReader reader = new BufferedReader(         // for reading input
-                new InputStreamReader(System.in));
-        String entry = null;
-        try {
-            System.out.print("User request:");
-            entry = reader.readLine();                     // read input line
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        test.AnalyzeEntry(entry);
-//        System.out.println("Category: " + test.category);
-//        System.out.println("Type: " + test.type);
-//        System.out.println("Amount: " + test.amount);
+        test.getUserRequest();
     }
 }
