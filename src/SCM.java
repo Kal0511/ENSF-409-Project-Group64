cb22cf3398a6168c1dfc9cd295d09f6a9d4860fa
AdamAbouelhassan <78115573+AdamAbouelhassan@users.noreply.github.com>
2021-03-27 12:33:30 -0600

    Create SCM.java

---
+++ src/SCM.java
@@ -0,0 +1,20 @@
+public class SCM {
+    public static void main(String[] args) {
+        Inventory test = new Inventory();
+        test.initializeConnection();
+
+        BufferedReader reader = new BufferedReader(         // for reading input
+                new InputStreamReader(System.in));
+        String entry = null;
+        try {
+            System.out.print("User request:");
+            entry = reader.readLine();                     // read input line
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        test.AnalyzeEntry(entry);
+        System.out.println("Category: " + test.category);
+        System.out.println("Type: " + test.type);
+        System.out.println("Amount: " + test.amount);
+    }
+}
