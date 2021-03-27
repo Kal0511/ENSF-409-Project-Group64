5caed8f5a03d850c28ace486e81f7d7898a4c98e
Khaled-Amer <khaled.amer216@gmail.com>
2021-03-27 23:39:33 +0400

    Merge branch 'main' of https://github.com/Kal0511/ENSF-409-Project-Group64 into main

--- src/Chair.java
+++ src/Chair.java
@@ -1,6 +1,15 @@
-    public class Chair extends Furniture {
+import java.sql.ResultSet;
+
+public class Chair extends Furniture {
         boolean legs;
         boolean arms;
         boolean seat;
         boolean cushion;
+        
+        public void checkRequest(ResultSet results) {
+        	while (results.next()){
+           
+                
+            }
+        }
     }
