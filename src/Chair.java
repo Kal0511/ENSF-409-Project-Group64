6730ddf098e81ff69c791990dcbb18f27435812c
AdamAbouelhassan <78115573+AdamAbouelhassan@users.noreply.github.com>
2021-03-28 13:08:08 -0600

    Update Chair.java

--- src/Chair.java
+++ src/Chair.java
@@ -9,125 +9,76 @@ public class Chair extends Furniture {
     boolean cushion = false;
     boolean check = false;
     int minPrice = 9999999;
-    int price=0;
+    int price = 0;
+    String result = "";
     StringBuffer itemIDs = new StringBuffer("");
     LinkedList<Integer> prices = new LinkedList<Integer>();
     LinkedList<String> IDs = new LinkedList<String>();
 
-    public Chair(LinkedList <String[]> results) {
+    public void checkRequest(LinkedList<String[]> results) {
 
-    }
-    
+        if (legs && arms && seat && cushion) {
 
-    public void checkRequest(LinkedList <String[]> results) {
-    	
-    	if(legs && arms && seat && cushion) {
-
-    		this.prices.add(price);
-    		this.IDs.add(itemIDs.toString());
+            this.prices.add(price);
+            this.IDs.add(itemIDs.toString());
 //    		System.out.println("combination found: "+itemIDs.toString());
-    		return;
-    	}
-    	if(results.size() < 1) {
+            return;
+        }
+        if (results.size() < 1) {
 //    		System.out.println("no combination found: "+itemIDs.toString());
-    		return;
-    	}
-    	
-    	for(int i=0; i<results.size(); i++) {
-    		String[] arr = results.get(i);
-    		
-    		boolean legsBefore = legs;
-    		boolean armsBefore = arms;
-    		boolean seatBefore = seat;
-    		boolean cushionBefore = cushion;
-    		
-    		if(arr[2].equals("Y")) {
-    			this.legs = true;
-    		}
-    		if(arr[3].equals("Y")) {
-    			this.arms = true;
-    		}
-    		if(arr[4].equals("Y")) {
-    			this.seat = true;
-    		}
-    		if(arr[5].equals("Y")) {
-    			this.cushion = true;
-    		}
-    		
-    		LinkedList <String[]> resultsRecursion = new LinkedList<String[]>() ;
-    		
-    		for (int j = 0; j < results.size(); j++){
-    			String[] copy = Arrays.copyOf(results.get(j), results.get(j).length);;
-    			resultsRecursion.add(copy);
-    		}
-    		price+= Integer.parseInt(results.get(i)[6]);
-    		itemIDs.append(" "+results.get(i)[0]);
-    		resultsRecursion.remove(i);
-    		checkRequest(resultsRecursion);
+            return;
+        }
 
-    		legs = legsBefore;
-    		arms = armsBefore;
-    		seat = seatBefore;
-    		cushion = cushionBefore;
-    		price-=Integer.parseInt(results.get(i)[6]);
-    		itemIDs.delete(itemIDs.length()-results.get(i)[0].length()-1, itemIDs.length());
-    	}
-    	
+        for (int i = 0; i < results.size(); i++) {
+            String[] arr = results.get(i);
+
+            boolean legsBefore = legs;
+            boolean armsBefore = arms;
+            boolean seatBefore = seat;
+            boolean cushionBefore = cushion;
+
+            if (arr[2].equals("Y")) {
+                this.legs = true;
+            }
+            if (arr[3].equals("Y")) {
+                this.arms = true;
+            }
+            if (arr[4].equals("Y")) {
+                this.seat = true;
+            }
+            if (arr[5].equals("Y")) {
+                this.cushion = true;
+            }
+
+            LinkedList<String[]> resultsRecursion = new LinkedList<String[]>();
+
+            for (int j = 0; j < results.size(); j++) {
+                String[] copy = Arrays.copyOf(results.get(j), results.get(j).length);
+                resultsRecursion.add(copy);
+            }
+            price += Integer.parseInt(results.get(i)[6]);
+            itemIDs.append(" " + results.get(i)[0]);
+            resultsRecursion.remove(i);
+            checkRequest(resultsRecursion);
+
+            legs = legsBefore;
+            arms = armsBefore;
+            seat = seatBefore;
+            cushion = cushionBefore;
+            price -= Integer.parseInt(results.get(i)[6]);
+            itemIDs.delete(itemIDs.length() - results.get(i)[0].length() - 1, itemIDs.length());
+        }
     }
-    
+
     public void checkPrices() {
-    	for(int i=0; i<prices.size(); i++) {
-    		System.out.print(prices.get(i)+" ");
-    	}
-    	System.out.println();
-    	for(int i=0; i<prices.size(); i++) {
-    		System.out.println(IDs.get(i)+" ");
-    	}
-    		
+        int tmp = 0;
+        for (int i = 0; i < prices.size(); i++) {
+            //System.out.print(prices.get(i) + " ");
+            if (prices.get(i) < minPrice) {
+                minPrice = prices.get(i);
+                tmp = i;
+            }
+        }
+        result += IDs.get(tmp).substring(1);
     }
-
-//    public Set<String[]> calcAllCombos(Set<String[]> originalSet) {
-//        Set<String[]>[] x;
-//        Set<Set<String[]>> sets = new HashSet<Set<String[]>>();
-//        if (originalSet.isEmpty())
-//            return null;
-//
-//        List<String[]> list = new ArrayList<String[]>(originalSet);
-//        String[] head = list.get(0);
-//        Set<String[]> rest = new HashSet<String[]>(list.subList(1, list.size()));
-//        for (Set<String[]> set : calcAllCombos(rest)) {
-//            Set<String[]> newSet = new HashSet<String[]>();
-//            newSet.add(head);
-//            newSet.addAll(set);
-//            sets.add(newSet);
-//            sets.add(set);
-//        }
-//        for (int i = 0; i < sets.size(); i++)
-//            if (checkValidCombo(sets.toArray(x)[i]))
-//                return sets.toArray(x)[i];
-//    }
-//
-//    public boolean checkValidCombo(Set<String[]> combo) {
-//        boolean ifLegs = false;
-//        boolean ifArms = false;
-//        boolean ifSeat = false;
-//        boolean ifCushion = false;
-//        for (String[] x : combo) {
-//            if (x[2] == "Y")
-//                ifLegs = true;
-//            if (x[3] == "Y")
-//                ifArms = true;
-//            if (x[4] == "Y")
-//                ifSeat = true;
-//            if (x[5] == "Y")
-//                ifCushion = true;
-//        }
-//        int price = 0;
-//        if (ifLegs && ifArms && ifSeat && ifCushion)
-//            for (String[] x : combo)
-//                price += Integer.parseInt(x[6]);
-//        if (price < minPrice)
-//            minPrice = price;
-//        return ifLegs && ifArms && ifSeat && ifCushion;
-//    }
 }
