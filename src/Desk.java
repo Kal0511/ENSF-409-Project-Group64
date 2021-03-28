711593554b1bd01a33c363faf514b2864695ec5c
AdamAbouelhassan <78115573+AdamAbouelhassan@users.noreply.github.com>
2021-03-28 16:57:40 -0600

    Update Desk.java

--- src/Desk.java
+++ src/Desk.java
@@ -7,16 +7,25 @@ public class Desk extends Furniture {
     boolean top = false;
     boolean drawer = false;
     boolean check = false;
+    int numOfLegs = 0;
+    int numOfTop = 0;
+    int numOfDrawer = 0;
     int minPrice = 9999999;
     int price = 0;
+    int amount;
     String result = "";
     StringBuffer itemIDs = new StringBuffer("");
     LinkedList<Integer> prices = new LinkedList<Integer>();
     LinkedList<String> IDs = new LinkedList<String>();
 
+    public Desk(int amount) {
+        this.amount = amount;
+    }
+
     public void checkRequest(LinkedList<String[]> results) {
 
-        if (legs && top && drawer) {
+        if (legs && top && drawer && numOfLegs >= amount && numOfTop >= amount && numOfDrawer >= amount) {
+
             this.prices.add(price);
             this.IDs.add(itemIDs.toString());
 //    		System.out.println("combination found: "+itemIDs.toString());
@@ -33,15 +42,22 @@ public class Desk extends Furniture {
             boolean legsBefore = legs;
             boolean topBefore = top;
             boolean drawerBefore = drawer;
+            int numOfLegsBefore = numOfLegs;
+            int numOfTopBefore = numOfTop;
+            int numOfDrawerBefore = numOfDrawer;
+
 
             if (arr[2].equals("Y")) {
                 this.legs = true;
+                numOfLegs++;
             }
             if (arr[3].equals("Y")) {
                 this.top = true;
+                numOfTop++;
             }
             if (arr[4].equals("Y")) {
                 this.drawer = true;
+                numOfDrawer++;
             }
 
             LinkedList<String[]> resultsRecursion = new LinkedList<String[]>();
@@ -58,6 +74,9 @@ public class Desk extends Furniture {
             legs = legsBefore;
             top = topBefore;
             drawer = drawerBefore;
+            numOfLegs = numOfLegsBefore;
+            numOfTop = numOfTopBefore;
+            numOfDrawer = numOfDrawerBefore;
             price -= Integer.parseInt(results.get(i)[5]);
             itemIDs.delete(itemIDs.length() - results.get(i)[0].length() - 1, itemIDs.length());
         }
@@ -72,6 +91,9 @@ public class Desk extends Furniture {
                 tmp = i;
             }
         }
-        result += IDs.get(tmp).substring(1);
+        if (!IDs.isEmpty()) {
+            result += IDs.get(tmp).substring(1);
+        }
+
     }
 }
