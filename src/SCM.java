1631580da751193f060f568150c41c78018ab5db
AdamAbouelhassan <78115573+AdamAbouelhassan@users.noreply.github.com>
2021-03-28 16:59:03 -0600

    Update SCM.java

--- src/SCM.java
+++ src/SCM.java
@@ -6,9 +6,22 @@ public class SCM {
         Inventory test = new Inventory();
         test.initializeConnection();
         test.getUserRequest();
-       	test.pullData();
-       	test.evaluvateRequest();
-       	test.updateDatabase();
-       	test.write();
+        test.pullData();
+        test.evaluvateRequest();
+        if (test.items.equals("")) {
+            String output = "";
+            output += "Order cannot be filled based on current inventory. Suggested manufacturers are ";
+            for (int i = 0; i < test.possibleManufacturersID.length; i++) {
+                if (test.possibleManufacturersID[i] != null) {
+                    output += test.possibleManufacturersID[i];
+                    output += ", ";
+                }
+            }
+            System.out.println(output.substring(0, output.length() - 2));
+            System.exit(1);
+        } else {
+            test.updateDatabase();
+            test.write();
+        }
     }
 }
