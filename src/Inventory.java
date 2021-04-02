af040c7c1542d5b7495a1d27ee3ae509e371800d
AdamAbouelhassan <78115573+AdamAbouelhassan@users.noreply.github.com>
2021-04-01 18:30:57 -0600

    documentation, slight optim

--- src/Inventory.java
+++ src/Inventory.java
@@ -2,40 +2,46 @@ import java.sql.*;
 import java.io.*;
 import java.util.*;
 
-/*
+/**
  * Inventory is a class that uses the SQL inventory database to process the
- * the user requested order for a piece of furniture, specified by category and type.
+ * the user requested order for an item of furniture, specified by category and type.
+ *
+ * @author Adam Abouelhassan
+ * @author Khaled Amer
+ * @author Kaumil Patel
+ * @author Dillon Matthews
+ * @version 2.1
+ * @since 1.0
  */
-
 public class Inventory {
-    private final String DBURL = "jdbc:mysql://localhost/inventory"; // store the database url information
-    private final String USERNAME = "adam"; // store the user's account username
-    private final String PASSWORD = "ensf409"; // store the user's account password
 
     private Connection dbConnect;
     private ResultSet results;
 
-    private String category;
-    private String type;
-    private int amount;
+    protected String category;
+    protected String type;
+    protected int amount;
 
     private String entry;
-    private ArrayList<String> items;
+    protected ArrayList<String> items;
     private int itemPrice;
 
     // possible manufacturers for each category of furniture
-    private String[] possibleManufacturersChair = { "002", "003", "004", "005" };
-    private String[] possibleManufacturersDesk = { "001", "002", "004", "005" };
-    private String[] possibleManufacturersLamp = { "002", "004", "005" };
-    private String[] possibleManufacturersFiling = { "002", "004", "005" };
+    private final String[] possibleManufacturersChair = {"002", "003", "004", "005"};
+    private final String[] possibleManufacturersDesk = {"001", "002", "004", "005"};
+    private final String[] possibleManufacturersLamp = {"002", "004", "005"};
+    private final String[] possibleManufacturersFiling = {"002", "004", "005"};
 
+    /**
+     * Constructor
+     */
     public Inventory() {
+        initializeConnection();
     }
 
-    /*
-     * getUserRequest is a a method with no return type and no arguments that
-     * prompts the user for input and uses a BufferedReader to to read in the user
-     * request. The user input is then passed to the AnalyzeEntry method to be
+    /**
+     * This prompts the user for an input and uses a BufferedReader to to read in the user's
+     * request. The user's input is then passed to the AnalyzeEntry method to be
      * parsed.
      */
     public void getUserRequest() {
@@ -47,64 +53,51 @@ public class Inventory {
             e.printStackTrace();
         }
         this.analyzeEntry(entry);
-        // System.out.println("Category: " + this.category);
-        // System.out.println("Type: " + this.type);
-        // System.out.println("Amount: " + this.amount);
     }
 
-    /*
-     * getDBURL is a method with no arguments that returns the String DBURL that
-     * connects to mySQL
+    /**
+     * Returns DBURL of desired MySQL database.
+     *
+     * @return
      */
-    public String getDBURL() {
-        return DBURL;
+    public String getDburl() {
+        return "jdbc:mysql://localhost/INVENTORY";
     }
 
-    /*
-     * getUSERNAME is a method with no arguments that returns the String USERNAME.
-     * This is the name of the current MySQL user
+    /**
+     * Returns the name of the MySQL user which the desired MySQL database belongs to.
+     *
+     * @return
      */
-    public String getUSERNAME() {
-        return USERNAME;
+    public String getUsername() {
+        return "adam";
     }
 
-    /*
-     * getPASSWORD is a method with no arguments that returns the String PASSWORD.
-     * This is the password to the respective mySQL User
+    /**
+     * Returns the password to the respective mySQL User.
+     *
+     * @return
      */
-    public String getPASSWORD() {
-        return PASSWORD;
+    public String getPassword() {
+        return "ensf409";
     }
 
-    /*
-     * initializeConnection is a method with no arguments and no return type that
-     * creates a connection from the program to the SQL database
+    /**
+     * Connects to database specified by getDburl(), uses getUsername() and getPassword() to access database.
      */
     public void initializeConnection() {
         try {
-            dbConnect = DriverManager.getConnection(this.DBURL, this.USERNAME, this.PASSWORD);
+            dbConnect = DriverManager.getConnection(this.getDburl(), this.getUsername(), this.getPassword());
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("Failed to connect to database");
         }
     }
 
-    /*
-     * close is a method that has no arguments and no return type. close closes the
-     * connection to the SQL database.
-     */
-    public void close() {
-        try {
-            results.close();
-            dbConnect.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /*
-     * AnalyzeEntry is a method that takes in the user inputted String, entry as an
-     * argument and parses it to determine the category, type of furniture, and
-     * amount that the user has requested. This method has no return type.
+    /**
+     * parses user entry into category, type, and amount.
+     *
+     * @param entry
      */
     public void analyzeEntry(String entry) {
         try {
@@ -123,62 +116,31 @@ public class Inventory {
         }
     }
 
-    /*
-     * toTitleCase is a method that takes one String as an argument, converts it to
-     * title case and returns it
-     */
-    public String toTitleCase(String x) {
-        StringBuilder titleCase = new StringBuilder(x.length());
-        boolean nextTitleCase = true;
-
-        for (char c : x.toCharArray()) {
-            if (Character.isSpaceChar(c)) {
-                nextTitleCase = true;
-            } else if (nextTitleCase) {
-                c = Character.toTitleCase(c);
-                nextTitleCase = false;
-            }
-            titleCase.append(c);
-        }
-        return titleCase.toString();
-    }
-
-    /*
-     * pullData has no arguments and no return type. method loads in all furniture
-     * belonging to the respective category and type into a ResultSet
+    /**
+     * Finds pieces of requested item using an adaptive query, stores results.
      */
     public void pullData() {
-        String query = null;
+        String query;
         try {
-//			if (category.equals("chair"))
-//				query = "SELECT * FROM Chair WHERE Type = ?";
-//			else if (category.equals("desk"))
-//				query = "SELECT * FROM Desk WHERE Type = ?";
-//			else if (category.equals("lamp"))
-//				query = "SELECT * FROM Lamp WHERE Type = ?";
-//			else if (category.equals("filing"))
             query = "SELECT * FROM $tableName WHERE Type = ?";
             query = query.replace("$tableName", category);
             PreparedStatement myStmt = dbConnect.prepareStatement(query);
             myStmt.setString(1, type);
             results = myStmt.executeQuery();
-            // while (results.next()) {
-            // System.out.println("Print results: " + results.getString("id"));
-            // }
-            // myStmt.close();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
     }
 
-    /*
+    /**
      * findManuID takes in a String array of manufacturer ID's as an argument and
      * replaces the contents of that string with the actual names of the respective
-     * manufacturers in the same order. This method returns nothing.
+     * manufacturers in the same order.
      *
+     * @throws SQLException
      */
     public void findManuID(String[] x) throws SQLException {
-        String query = null;
+        String query;
         ResultSet resultSet = null;
         for (int i = 0; i < x.length; i++) {
             try {
@@ -189,72 +151,94 @@ public class Inventory {
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
-            while (resultSet.next()) {
-                x[i] = resultSet.getString("Name");
+            if (resultSet != null) {
+                while (resultSet.next()) {
+                    x[i] = resultSet.getString("Name");
+                }
             }
         }
     }
 
-    /*
-     * evaluateRequest is a method with no arguments and no return type. method goes
-     * through the results ResultSet and saves all the properties of each piece of
-     * furniture to their own String arrays that are added to a the resultList
-     * LinkedList
+    /**
+     * method iterates through the results found by pullDat() and saves all the properties of each piece of
+     * furniture to their own String array which are added to a more in depth list of pieces
+     *
+     * @throws SQLException
      */
     public void evaluateRequest() throws SQLException {
-        if (category.equals("chair")) {
-            ArrayList<Chair> list = new ArrayList<Chair>();
-            while (results.next()) {
-                Chair temp = new Chair(results.getString("ID"), strToInt(results.getString("Legs")),
-                        strToInt(results.getString("Arms")), strToInt(results.getString("Seat")),
-                        strToInt(results.getString("Cushion")), results.getInt("Price"));
-                list.add(temp);
-            }
-            findManuID(possibleManufacturersChair);
-            Chair best = Chair.processRequest(list, amount);
-            this.items = best.getIDs();
-            this.itemPrice = best.getPrice();
+        switch (category) {
+            case "chair": {
+                ArrayList<Chair> list = new ArrayList<>();
+                while (results.next()) {
+                    Chair temp = new Chair(results.getString("ID"), strToInt(results.getString("Legs")),
+                            strToInt(results.getString("Arms")), strToInt(results.getString("Seat")),
+                            strToInt(results.getString("Cushion")), results.getInt("Price"));
+                    list.add(temp);
+                }
+                findManuID(possibleManufacturersChair);
+                Chair best = Chair.processRequest(list, amount);
+                this.items = best.getIDs();
+                this.itemPrice = best.getPrice();
 
-        } else if (category.equals("desk")) {
-            ArrayList<Desk> list = new ArrayList<Desk>();
-            while (results.next()) {
-                Desk temp = new Desk(results.getString("ID"), strToInt(results.getString("Legs")),
-                        strToInt(results.getString("Top")), strToInt(results.getString("Drawer")),
-                        results.getInt("Price"));
-                list.add(temp);
+                break;
             }
-            findManuID(possibleManufacturersDesk);
-            Desk best = Desk.processRequest(list, amount);
-            this.items = best.getIDs();
-            this.itemPrice = best.getPrice();
-        } else if (category.equals("filing")) {
-            ArrayList<Filing> list = new ArrayList<Filing>();
-            while (results.next()) {
-                Filing temp = new Filing(results.getString("ID"), strToInt(results.getString("Rails")),
-                        strToInt(results.getString("Cabinet")), strToInt(results.getString("Drawers")),
-                        results.getInt("Price"));
-                list.add(temp);
+            case "desk": {
+                ArrayList<Desk> list = new ArrayList<>();
+                while (results.next()) {
+                    Desk temp = new Desk(results.getString("ID"), strToInt(results.getString("Legs")),
+                            strToInt(results.getString("Top")), strToInt(results.getString("Drawer")),
+                            results.getInt("Price"));
+                    list.add(temp);
+                }
+                findManuID(possibleManufacturersDesk);
+                Desk best = Desk.processRequest(list, amount);
+                if (best != null) {
+                    this.items = best.getIDs();
+                }
+                if (best != null) {
+                    this.itemPrice = best.getPrice();
+                }
+                break;
             }
-            findManuID(possibleManufacturersFiling);
-            Filing best = Filing.processRequest(list, amount);
-            this.items = best.getIDs();
-            this.itemPrice = best.getPrice();
-        } else if (category.equals("lamp")) {
-            ArrayList<Lamp> list = new ArrayList<Lamp>();
-            while (results.next()) {
-                Lamp temp = new Lamp(results.getString("ID"), strToInt(results.getString("Base")),
-                        strToInt(results.getString("Bulb")), results.getInt("Price"));
-                list.add(temp);
+            case "filing": {
+                ArrayList<Filing> list = new ArrayList<>();
+                while (results.next()) {
+                    Filing temp = new Filing(results.getString("ID"), strToInt(results.getString("Rails")),
+                            strToInt(results.getString("Cabinet")), strToInt(results.getString("Drawers")),
+                            results.getInt("Price"));
+                    list.add(temp);
+                }
+                findManuID(possibleManufacturersFiling);
+                Filing best = Filing.processRequest(list, amount);
+                this.items = best.getIDs();
+                this.itemPrice = best.getPrice();
+                break;
             }
-            findManuID(possibleManufacturersLamp);
-            Lamp best = Lamp.processRequest(list, amount);
-            this.items = best.getIDs();
-            this.itemPrice = best.getPrice();
+            case "lamp": {
+                ArrayList<Lamp> list = new ArrayList<>();
+                while (results.next()) {
+                    Lamp temp = new Lamp(results.getString("ID"), strToInt(results.getString("Base")),
+                            strToInt(results.getString("Bulb")), results.getInt("Price"));
+                    list.add(temp);
+                }
+                findManuID(possibleManufacturersLamp);
+                Lamp best = Lamp.processRequest(list, amount);
+                this.items = best.getIDs();
+                this.itemPrice = best.getPrice();
+                break;
+            }
         }
         checkIfOrderFilled();
         results.close();
     }
 
+    /**
+     * strToInt() method simply returns an int, either 1 or 0 to indicate a 'Y' or 'N' of components in the
+     * mysql database to increase readability of code
+     *
+     * @param check
+     * @return
+     */
     public int strToInt(String check) {
         if (check.equals("Y")) {
             return 1;
@@ -263,71 +247,64 @@ public class Inventory {
         }
     }
 
-    /*
-     * updateDatabase is a method that has no arguments and no returns nothing This
-     * method updates the database by deleting the items that were purchased.
+    /**
+     * This method updates the database by deleting the items that were purchased, uses an adaptive query
+     * to identify which elements from the database to delete.
      */
     public void updateDatabase() {
-        String query = null;
+        String query;
         PreparedStatement myStmt = null;
         try {
-//			if (category.equals("chair"))
-//				query = "DELETE FROM chair WHERE ID = ?";
-//			else if (category.equals("desk"))
-//				query = "DELETE FROM desk WHERE ID = ?";
-//			else if (category.equals("lamp"))
-//				query = "DELETE FROM lamp WHERE ID = ?";
-//			else if (category.equals("filing"))
             query = "DELETE FROM $tableName WHERE ID = ?";
             query = query.replace("$tableName", category);
-            for (int i = 0; i < items.size(); i++) {
+            for (String item : items) {
                 myStmt = dbConnect.prepareStatement(query);
-                myStmt.setString(1, items.get(i));
+                myStmt.setString(1, item);
                 myStmt.execute();
             }
-            myStmt.close();
+            if (myStmt != null) {
+                myStmt.close();
+            }
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
     }
 
-    /*
-     * checkIfOrderFilled is a method with no arguments that checks to see if it is
-     * possible to fill the order. if it cannot be fulfilled it outputs a message.
-     * returns nothing
+    /**
+     * checks to see if it is possible to fill the requested order,
+     * if it cannot be fulfilled it outputs a message and exits.
      */
     public void checkIfOrderFilled() {
         if (items.get(0) == null) {
-            String output = "";
-            output += "Order cannot be filled based on current inventory. Suggested manufacturers are ";
+            StringBuilder output = new StringBuilder();
+            output.append("Order cannot be filled based on current inventory. Suggested manufacturers are ");
             if (category.equals("chair"))
-                for (int i = 0; i < possibleManufacturersChair.length; i++) {
-                    output += possibleManufacturersChair[i];
-                    output += ", ";
+                for (String s : possibleManufacturersChair) {
+                    output.append(s);
+                    output.append(", ");
                 }
             if (category.equals("desk"))
-                for (int i = 0; i < possibleManufacturersDesk.length; i++) {
-                    output += possibleManufacturersDesk[i];
-                    output += ", ";
+                for (String s : possibleManufacturersDesk) {
+                    output.append(s);
+                    output.append(", ");
                 }
             if (category.equals("lamp"))
-                for (int i = 0; i < possibleManufacturersLamp.length; i++) {
-                    output += possibleManufacturersLamp[i];
-                    output += ", ";
+                for (String s : possibleManufacturersLamp) {
+                    output.append(s);
+                    output.append(", ");
                 }
             if (category.equals("filing"))
-                for (int i = 0; i < possibleManufacturersFiling.length; i++) {
-                    output += possibleManufacturersFiling[i];
-                    output += ", ";
+                for (String s : possibleManufacturersFiling) {
+                    output.append(s);
+                    output.append(", ");
                 }
             System.out.println(output.substring(0, output.length() - 2));
             System.exit(1);
         }
     }
 
-    /*
-     * write is a method with no arguments that writes the order details to a file
-     * called orderForm.txt. no return type.
+    /**
+     * Writes the order details to a file called orderForm.txt.
      */
     public void write() {
         System.out.print("Output: Purchase ");
@@ -346,12 +323,10 @@ public class Inventory {
             fo.write("Date:\n");
             fo.write("\nOriginal Request: " + entry + "\n");
             fo.write("\nItmes Ordered:\n");
-            for (int i = 0; i < items.size(); i++)
-                fo.write("ID: " + items.get(i) + "\n");
+            for (String item : items) fo.write("ID: " + item + "\n");
             fo.write("\nTotal Price: $" + itemPrice);
             fo.close();
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
-}
