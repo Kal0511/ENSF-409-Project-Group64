8495d49ae3113b335a50cf7227a3f77f741f536c
DillonMatthews11 <81485381+DillonMatthews11@users.noreply.github.com>
2021-04-03 14:01:37 -0600

    Update Furniture.java

--- src/Furniture.java
+++ src/Furniture.java
@@ -9,14 +9,16 @@ public class Furniture {
 	protected ArrayList<String> IDs;
 	protected int totalPrice;
 	protected int completeSet;
-
+	/** 
+	*Constructor
+	*/
 	public Furniture(String _ID, int _price, int set) {
 		this.IDs = new ArrayList<>();
 		this.IDs.add(_ID);
 		this.totalPrice = _price;
 		this.completeSet = set;
 	}
-
+	
 	public Furniture(ArrayList<String> _IDs, int _price, int set) {
 		this.IDs = _IDs;
 		this.totalPrice = _price;
