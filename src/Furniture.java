9b3e868f922ca698930446c58a3c83f32d3eb3a8
Kaumil Patel <spdmode@gmail.com>
2021-05-11 11:57:33 -0600

    Update Furniture.java

--- src/Furniture.java
+++ src/Furniture.java
@@ -1,57 +1,56 @@
 import java.util.ArrayList;
 
 /**
- * Furniture is a parent class that contains
- * variables common to all different categories
- * of furniture.
+ * Furniture is a parent class that contains variables common to all different
+ * categories of furniture.
  */
 public class Furniture {
-    protected ArrayList<String> IDs;
-    protected int totalPrice;
-    protected int completeSet;
+	protected ArrayList<String> IDs;
+	protected int totalPrice;
+	protected int completeSet;
 
-    /**
-     * Constructor
-     *
-     * @param _ID ID number of piece
-     * @param _price price of piece
-     * @param set number of items
-     */
-    public Furniture(String _ID, int _price, int set) {
-        this.IDs = new ArrayList<>();
-        this.IDs.add(_ID);
-        this.totalPrice = _price;
-        this.completeSet = set;
-    }
+	/**
+	 * Constructor
+	 *
+	 * @param _ID    ID number of piece
+	 * @param _price price of piece
+	 * @param set    number of items
+	 */
+	public Furniture(String _ID, int _price, int set) {
+		this.IDs = new ArrayList<>();
+		this.IDs.add(_ID);
+		this.totalPrice = _price;
+		this.completeSet = set;
+	}
 
-    /**
-     * Constructor
-     *
-     * @param _IDs ID numbers of pieces
-     * @param _price price of piece
-     * @param set number of items
-     */
-    public Furniture(ArrayList<String> _IDs, int _price, int set) {
-        this.IDs = _IDs;
-        this.totalPrice = _price;
-        this.completeSet = set;
-    }
+	/**
+	 * Constructor
+	 *
+	 * @param _IDs   ID numbers of pieces
+	 * @param _price price of piece
+	 * @param set    number of items
+	 */
+	public Furniture(ArrayList<String> _IDs, int _price, int set) {
+		this.IDs = _IDs;
+		this.totalPrice = _price;
+		this.completeSet = set;
+	}
 
-    /**
-     * Getter for IDS
-     *
-     * @return IDs
-     */
-    public ArrayList<String> getIDs() {
-        return this.IDs;
-    }
+	/**
+	 * Getter for IDS
+	 *
+	 * @return IDs
+	 */
+	public ArrayList<String> getIDs() {
+		return this.IDs;
+	}
 
-    /**
-     * Getter for price
-     *
-     * @return price
-     */
-    public int getPrice() {
-        return this.totalPrice;
-    }
+	/**
+	 * Getter for price
+	 *
+	 * @return price
+	 */
+	public int getPrice() {
+		return this.totalPrice;
+	}
 }
