5cab2a9f9ef5bfc4a3a655cffc7cb1f5486fa5cb
Kaumil Patel <spdmode@gmail.com>
2021-05-11 11:57:43 -0600

    Update SCM.java

--- src/SCM.java
+++ src/SCM.java
@@ -1,26 +1,28 @@
 import java.sql.SQLException;
 
 /**
- * SCM is a program which takes a user request as input from the terminal window and searches mysql
- * database for requested items, it outputs a order form .txt file if the order can be filled, otherwise
- * outputs suggested manufacturers for the requested item.
+ * SCM is a program which takes a user request as input from the terminal window
+ * and searches mysql database for requested items, it outputs a order form .txt
+ * file if the order can be filled, otherwise outputs suggested manufacturers
+ * for the requested item.
  */
 public class SCM {
 
-    /**
-     * This initializes the Inventory class and calls all required methods to implement useful functionality.
-     *
-     * @param args Optional command-line argument
-     * @throws SQLException For handling exceptions regarding MySQL
-     */
-    public static void main(String[] args) throws SQLException {
-        Inventory test = new Inventory();
-        test.initializeConnection();
-        test.getUserRequest();
-        test.pullData();
-        test.evaluateRequest();
-        test.checkIfOrderFilled();
-        test.updateDatabase();
-        test.write();
-    }
+	/**
+	 * This initializes the Inventory class and calls all required methods to
+	 * implement useful functionality.
+	 *
+	 * @param args Optional command-line argument
+	 * @throws SQLException For handling exceptions regarding MySQL
+	 */
+	public static void main(String[] args) throws SQLException {
+		Inventory test = new Inventory();
+		test.initializeConnection();
+		test.getUserRequest();
+		test.pullData();
+		test.evaluateRequest();
+		test.checkIfOrderFilled();
+		test.updateDatabase();
+		test.write();
+	}
 }
